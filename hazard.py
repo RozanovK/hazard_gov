3e147c1024009b4f462640c955760e8c635b8c58
Kateryna Rozanova <rozanovk@icm.edu.pl>
2017-08-07 16:58:54 +0200

    Namespace fix

--- hazard.py
+++ hazard.py
@@ -1,17 +1,20 @@
 from urllib2 import urlopen
 import xml.etree.ElementTree as ET
 from jinja2 import Environment, FileSystemLoader
+import re
 
 
-
-content = urlopen('https://www.hazard.mf.gov.pl/api/Register').read()
+content = urlopen("https://www.hazard.mf.gov.pl/api/Register").read()
 root = ET.fromstring(content)
 values = []
 
-for child in root.findall('{http://www.hazard.mf.gov.pl/2017/03/21/}PozycjaRejestru'):
-	lp = child.get('Lp')
-	zone = child.find('{http://www.hazard.mf.gov.pl/2017/03/21/}AdresDomeny').text
-	date = child.find('{http://www.hazard.mf.gov.pl/2017/03/21/}DataWpisu').text
+m = re.match('\{.*\}', root.tag)
+namespace = m.group(0) if m else ''
+
+for child in root.findall(namespace + "PozycjaRejestru"):
+	lp = child.get("Lp")
+	zone = child.find(namespace + "AdresDomeny").text
+	date = child.find(namespace + "DataWpisu").text
 	values.append([lp, date, zone])
 
 env = Environment(loader=FileSystemLoader('templates'))
