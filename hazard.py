53df2ac5f93827a961b9f1a1ce5444c1f9280c51
Kateryna Rozanova <rozanovk@icm.edu.pl>
2017-08-09 11:54:05 +0200

    Separation of functionality

--- hazard.py
+++ hazard.py
@@ -3,30 +3,49 @@ import xml.etree.ElementTree as ET
 from jinja2 import Environment, FileSystemLoader
 import configparser
 
-settings = configparser.ConfigParser()
-settings.read('hazard.ini')
-api_time = settings.get('API', 'API_TIME')
-api_register = settings.get('API', 'API_REGISTER')
-last_modif = settings.get('Dates', 'Last_Modif')
-output_file = settings.get('Paths', 'Output_File')
+CONFIG_FILE= 'hazard.ini'
 
-content = urlopen(api_register).read()
-root = ET.fromstring(content)
-values = []
-namespace = root.tag.split("Rejestr", 1)[0]
 
-for child in root.findall(namespace + "PozycjaRejestru"):
-	lp = child.get("Lp")
-	zone = child.find(namespace + "AdresDomeny").text
-	date = child.find(namespace + "DataWpisu").text
-	values.append([lp, date, zone])
+def get_config(config_file=CONFIG_FILE):
+	settings = configparser.ConfigParser()
+	settings.read(config_file)
+	api_time = settings.get('API', 'API_TIME')
+	api_register = settings.get('API', 'API_REGISTER')
+	last_modif = settings.get('Dates', 'Last_Modif')
+	output_file = settings.get('Paths', 'Output_File')
+	return api_time, api_register, last_modif, output_file
 
-env = Environment(loader=FileSystemLoader('templates'))
-template = env.get_template('template.html')
-output = template.render(values=values)
 
-with open("hazard", "wb") as f:
-    f.write(output)
+def get_data(api_register):
+	content = urlopen(api_register).read()
+	root = ET.fromstring(content)
+	values = []
+	namespace = root.tag.split("Rejestr", 1)[0]
+
+	for child in root.findall(namespace + "PozycjaRejestru"):
+		lp = child.get("Lp")
+		zone = child.find(namespace + "AdresDomeny").text
+		date = child.find(namespace + "DataWpisu").text
+		values.append([lp, date, zone])
+	return values
+
+
+def generate_file(output_file, api_register):
+	values = get_data(api_register)
+	env = Environment(loader=FileSystemLoader('templates'))
+	template = env.get_template('template.html')
+	output = template.render(values=values)
+
+	with open(output_file, "wb") as f:
+    		f.write(output)
+		f.close()
+
+
+
+if __name__ == "__main__":
+
+	api_time, api_register, last_modif, output_file = get_config()
+	generate_file(output_file, api_register)
 
 
 
