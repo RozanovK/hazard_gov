b32c2e494ef464b115c585459be6b05d314cc05e
Kateryna Rozanova <rozanovk@icm.edu.pl>
2017-08-09 14:13:21 +0200

    Add time comparing feature

--- hazard.py
+++ hazard.py
@@ -2,18 +2,19 @@ from urllib2 import urlopen
 import xml.etree.ElementTree as ET
 from jinja2 import Environment, FileSystemLoader
 import configparser
+from datetime import datetime
+import os
 
 CONFIG_FILE= 'hazard.ini'
-
+TIME_F = '%Y-%m-%dT%H:%M:%S'
 
 def get_config(config_file=CONFIG_FILE):
 	settings = configparser.ConfigParser()
 	settings.read(config_file)
 	api_time = settings.get('API', 'API_TIME')
 	api_register = settings.get('API', 'API_REGISTER')
-	last_modif = settings.get('Dates', 'Last_Modif')
 	output_file = settings.get('Paths', 'Output_File')
-	return api_time, api_register, last_modif, output_file
+	return api_time, api_register, output_file
 
 
 def get_data(api_register):
@@ -29,8 +30,20 @@ def get_data(api_register):
 		values.append([lp, date, zone])
 	return values
 
+def get_r_time(api_time):    #get time of last register modification
+	content = urlopen(api_time).read()
+	root = ET.fromstring(content)
+	time_register = datetime.strptime(root.text, TIME_F)
+	return time_register
 
-def generate_file(output_file, api_register):
+def get_f_time(output_file):	#get last modification of output file
+	if os.path.isfile(output_file):
+		last_modified_date = datetime.fromtimestamp(os.path.getmtime(output_file))
+	else:
+    		last_modified_date = datetime.fromtimestamp(0)
+	return last_modified_date
+
+def generate_file(output_file, api_register, config_file=CONFIG_FILE):
 	values = get_data(api_register)
 	env = Environment(loader=FileSystemLoader('templates'))
 	template = env.get_template('template.html')
@@ -38,14 +51,35 @@ def generate_file(output_file, api_register):
 
 	with open(output_file, "wb") as f:
     		f.write(output)
-		f.close()
+	f.close()
 
+	def modification_time(config_file):
+		now = datetime.now().strftime(TIME_F)
+
+		settings = configparser.ConfigParser()
+		settings.read(config_file)
+		settings.set('Dates', 'Last_Modif', now)
+
+		with open(config_file, 'w') as f:
+			settings.write(f)
+		f.close()
+	modification_time(config_file)
+	
+
+def check_time(api_time, output_file, api_register, config_file=CONFIG_FILE):
+	r_t = get_r_time(api_time)
+	f_t = get_f_time(output_file)
+	if r_t > f_t:
+		generate_file(output_file, api_register, config_file)
+	else:
+		print ("No updates found!")
+		 
 
 
 if __name__ == "__main__":
 
-	api_time, api_register, last_modif, output_file = get_config()
-	generate_file(output_file, api_register)
+	api_time, api_register, output_file = get_config()
+	check_time(api_time, output_file, api_register)
 
 
 
