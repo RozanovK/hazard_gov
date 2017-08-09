5f192d75cf7ba41ab6b5f2aafd853f0d9614fd1f
Kateryna Rozanova <rozanovk@icm.edu.pl>
2017-08-09 17:24:17 +0200

    Add ability to set zone file

--- hazard.py
+++ hazard.py
@@ -31,7 +31,8 @@ def get_config(config_file=CONFIG_FILE):
 	api_register = settings.get('API', 'api_register')
 	output_file = settings.get('Paths', 'output_file')
 	log_file = settings.get('Paths', 'log_file')
-	return api_time, api_register, output_file, log_file
+	zone_file = settings.get('Paths', 'zone_file')
+	return api_time, api_register, output_file, log_file, zone_file
 
 def get_data(api_register):
 	content = urlopen(api_register).read()
@@ -60,20 +61,20 @@ def get_f_time(output_file):	#get last modification of output file
 		logger.info("No output file found!")
 	return last_modified_date
 
-def check_time(api_time, output_file, api_register, config_file=CONFIG_FILE):
+def check_time(api_time, output_file, zone_file, api_register, config_file=CONFIG_FILE):
 	r_t = get_r_time(api_time)
 	f_t = get_f_time(output_file)
 	if r_t > f_t:
 		logger.info("Generating file...")
-		generate_file(output_file, api_register, config_file)
+		generate_file(output_file, api_register,zone_file, config_file)
 	else:
 		logger.info("No updates found!")
 
-def generate_file(output_file, api_register, config_file=CONFIG_FILE):
+def generate_file(output_file, api_register,zone_file, config_file=CONFIG_FILE):
 	values = get_data(api_register)
 	env = Environment(loader=FileSystemLoader('templates'))
 	template = env.get_template('template.html')
-	output = template.render(values=values)
+	output = template.render(values=values, zone_file=zone_file)
 
 	with open(output_file, "wb") as f:
     		f.write(output)
@@ -85,9 +86,9 @@ if __name__ == "__main__":
 	logging.basicConfig(level=logging.INFO)
 	logger = logging.getLogger(__name__)
 	
-	api_time, api_register, output_file, log_file = get_config()
+	api_time, api_register, output_file, log_file, zone_file = get_config()
 	log(logger, log_file)
-	check_time(api_time, output_file, api_register)
+	check_time(api_time, output_file, zone_file, api_register)
 
 
 
